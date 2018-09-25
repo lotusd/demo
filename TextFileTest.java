bf4b72a9546578a852b76c5b96b8f7ab088f0da6
lotusd <41987598+lotusd@users.noreply.github.com>
2018-09-25 14:55:40 +0800

    save data

---
+++ TextFileTest.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.time.*;
+
+public class TextFileTest {
+    public static void main(String[] args) throws IOException {
+        Employee[] staff = new Employee[3];
+
+        staff[0] = new Employee("Carl Cracker", 75000, 1987, 12, 15);
+        staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+        staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
+
+        try (PrintWriter out = new PrintWriter("employee.dat", "UTF-8")) {
+            writeData(staff, out);
+        }
+    }
+
+    private static void writeData(Employee[] employees, PrintWriter out) {
+        out.println(employees.length);
+
+        for (Employee e : employees)
+            writeEmployee(out, e);
+    }
+
+    public static void writeEmployee(PrintWriter out, Employee e) {
+        out.println(e.getName() + "|" + e.getSalary() + "|" + e.getHireDay());
+    }
+}
+
+class Employee implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee() {
+    }
+
+    public Employee(String n, double s, int year, int month, int day) {
+        name = n;
+        salary = s;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    /**
+     * Raises the salary of this employee.
+     * 
+     * @byPercent the percentage of the raise
+     */
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    public String toString() {
+        return getClass().getName() + "[name=" + name + ",salary=" + salary + ",hireDay=" + hireDay + "]";
+    }
+}
