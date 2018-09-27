6ec3e6424d4fe771e1bec6680a4590d0e3e5103f
lotusd <41987598+lotusd@users.noreply.github.com>
2018-09-27 13:57:34 +0800

    change 2 IJ IDE

---
+++ src/com/jian/Employee.java
@@ -0,0 +1,46 @@
+package com.jian;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+class Employee implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee() {
+    }
+
+    public Employee(String n, double s, int year, int month, int day) {
+        name = n;
+        salary = s;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    /**
+     * Raises the salary of this employee.
+     *
+     * @byPercent the percentage of the raise
+     */
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    public String toString() {
+        return getClass().getName() + "[name=" + name + ",salary=" + salary + ",hireDay=" + hireDay + "]";
+    }
+}
--- src/com/jian/TextFileTest.java
+++ src/com/jian/TextFileTest.java
@@ -1,3 +1,5 @@
+package com.jian;
+
 import java.io.*;
 import java.time.*;
 import java.util.*;
@@ -59,44 +61,4 @@ public class TextFileTest {
     }
 }
 
-class Employee implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private String name;
-    private double salary;
-    private LocalDate hireDay;
 
-    public Employee() {
-    }
-
-    public Employee(String n, double s, int year, int month, int day) {
-        name = n;
-        salary = s;
-        hireDay = LocalDate.of(year, month, day);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public LocalDate getHireDay() {
-        return hireDay;
-    }
-
-    /**
-     * Raises the salary of this employee.
-     * 
-     * @byPercent the percentage of the raise
-     */
-    public void raiseSalary(double byPercent) {
-        double raise = salary * byPercent / 100;
-        salary += raise;
-    }
-
-    public String toString() {
-        return getClass().getName() + "[name=" + name + ",salary=" + salary + ",hireDay=" + hireDay + "]";
-    }
-}
